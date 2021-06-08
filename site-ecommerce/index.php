44184cf896d60ced7bc5f20aa50eb5226278aff0
zineb-ch <72198154+zineb-ch@users.noreply.github.com>
2021-06-08 14:25:18 +0200

    Site complet

---
+++ site-ecommerce/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"/>
+		<link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Roboto:500"/>
+		<link rel="stylesheet" type="text/css" href="css/main.css"/>
+		<title>Nom du site à trouver</title>
+	</head>
+	<body>
+		<header>
+			<a href="#" id="logo"></a>
+			<div id="hamburger-content">
+				<nav>
+					<ul>
+						<li><a href="#">Boutique</a></li>
+						<li><a href="#">Promos</a></li>
+						<li><a href="#">Panier</a></li>
+					</ul>
+				</nav>
+				<a href="#" class="button button-sign-up">S'inscrire</a>
+				<a href="#" class="button button-sign-in">Se connecter</a>
+			</div>
+			<button id="hamburger-button">&#9776;</button>
+			<div id="hamburger-sidebar">
+				<div id="hamburger-sidebar-header"></div>
+				<div id="hamburger-sidebar-body"></div>
+			</div>
+			<div id="humburger-overlay"></div>
+		</header>
+		<div id="global">
+			<h1>Site e-commerce</h1>
+		</div>
+		<script type="text/javascript" src="js/main.js"></script>
+	</body>
+</html>
